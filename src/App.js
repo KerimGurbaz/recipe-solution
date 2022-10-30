77a57dc07cda1a2a8d00589ccb048d0a2845464d
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-10-30 21:51:55 +0100

    private router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
-import AppRouter from './router/AppRouter'
+import AppRouter from './router/AppRouter';
+import theme from './components/globalStyles/theme'
+import { ThemeProvider } from 'styled-components';
 
 const App = () => {
   return (
-    <div>
-     <AppRouter/>
-    </div>
+    <ThemeProvider theme={theme}>
+    <AppRouter/>
+    </ThemeProvider>
   )
 }
 
