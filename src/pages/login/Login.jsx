04b77982193319c64fd9a05924418e8558f5297b
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-10-30 22:53:45 +0100

    session storage

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,41 @@
-import LoginStyle from "./Login.style";
+import {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from "./Login.style";
+import meal from "../../assets/meal.gif";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const userInfo = {
+    username: "Kerim",
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(userInfo));
+    navigate(-1);
+  };
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={meal} />
+        <Header>Recipe</Header>
+
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput type="text" placeholder="username" required />
+          <StyledInput type="password" placeholder="password" required />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
 };
 
 export default Login;
