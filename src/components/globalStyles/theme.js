9f5a8046e146cd2258d8544f6f588c673e9741db
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-10-31 11:47:23 +0100

    updated

--- src/components/globalStyles/theme.js
+++ src/components/globalStyles/theme.js
@@ -1,8 +1,13 @@
 const theme = {
     colors: {
+        navbarBgColor:'#e1f1dd',
+        mainColor:'#00adb5',
+        logocolor:'#393e46',
+        linkcolor:'#e02475e'
 
     },
     screens:{
+        lg:'810px',
 
     }
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
 import NavbarStyle from "./Navbar.style";
+import Nav, { Logo, Menu, MenuLink, Hamburger } from "./Navbar.style";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
 const Navbar = () => {
-  return <div>NAvbar</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <Nav justify="space-between" wrap="wrap">
+      <Logo>
+        <i>Kerim -- </i>
+        <span>Recipe</span>
+      </Logo>
+      <Hamburger onClick={() => !isOpen}>
+        <GiHamburgerMenu />
+      </Hamburger>
+      <Menu isOpen={isOpen}>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="about">About</MenuLink>
+        <MenuLink to="register">Register</MenuLink>
+        <MenuLink to="login">Logout</MenuLink>
+      </Menu>
+    </Nav>
+  );
 };
 
 export default Navbar;
